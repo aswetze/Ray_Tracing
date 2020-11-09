e0ef65f39b1d2086e4bd306e4f1010c8c38eebb6
aswetze <60395867+aswetze@users.noreply.github.com>
2020-11-09 12:31:50 -0500

    Graph is now non-linear but needs to be set up to be discontinous

--- Coleman_RT_Julia (1).jl
+++ Coleman_RT_Julia (1).jl
@@ -1,5 +1,6 @@
 using OrdinaryDiffEq, DiffEqGPU, Plots, ODEInterfaceDiffEq, ODEInterfaceDiffEq
 
+#Checked for correctness against Coleman_RT_Python.ipynb
 function qpne(r, nm, hm, ym)
     hm_meters = hm * 10^3
     ym_meters = ym * 10^3
@@ -10,15 +11,16 @@ function qpne(r, nm, hm, ym)
 
     if (r > r_base && r < r_top)
         a = (r - r_max) / ym_meters
-        ne = nm * (1 - a^2)
-        dne_dr = -2 * nm * a / ym_meters
+        ne = nm * (1. - a^2)
+        dne_dr = -2. * nm * a / ym_meters
     else
-        ne = 1 * 10^-31
-        dne_dr = ne
+        ne = 1. * 10^-31
+        dne_dr = 1. * 10^-31
     end
     return ne, dne_dr
 end
 
+#Checked for correctness against Coleman_RT_Python.ipynb
 function index_refraction_no_b(r, freq, iono_params)
     nm = iono_params[2]
     hm = iono_params[3]
@@ -37,13 +39,14 @@ function raytrace!(du, u, p, t)
     q = u[1]
     r = u[2]
     θ = u[3]
-    μ2 = u[4]
-    freq = 8.e6
 
-    du[1] = index_refraction_no_b(r, freq, ["QP", 5.e11, 300, 50])[2]*q/2 + (μ2-q^2)/r
+    freq = 4.e6
+    μ2, dμ2_dr = index_refraction_no_b(r, freq, ["QP", 5.e11, 300, 50])
+
+    du[1] = dμ2_dr/2 + (μ2-q^2)/r
     du[2] = q
     du[3] = sqrt(μ2-q^2)/r
-    du[4] = index_refraction_no_b(r, freq, ["QP", 5.e11, 300, 50])[2]*q
+
 end
 
 
@@ -57,13 +60,20 @@ Q0 = sin(π/4)
 u0 = Float64[Q0; r0; θ0; μ20]
 tspan = (0.0f0,12000.f0)
 prob = ODEProblem(raytrace!, u0, tspan)
-@time sol = solve(prob, Euler(), dt = 10e3, abstol=1e-6, dtmax = 10e3)
-#println("sol1 = ")
+@time sol = solve(prob, Euler(), )
 #println(sol[1,:])
 #println("sol2 = ")
-#println(sol[2,:] .- 6371e3)
+#println(sol[2,:])
 #println("sol3 = ")
 #println(sol[3,:])
 #println("sol.t")
 #println(sol.t)
+#println(sol[2,:].-6371e3)
 plot(sol.t .* 6371e3, sol[2,:] .- 6371e3)
+
+
+rTest = collect(6371.:  1.: 6700.) * 1000.
+for ii in rTest
+    mu, dmudr = index_refraction_no_b(rTest, 4.e6, ["QP", 5.e11, 300, 50])
+    #println(ii, " ", mu, " ", dmudr)
+end
